dc570d87767304e9955b39dfcccd89c9a9b4c6c4
Tivsue Tavershima <tivsuetavershima@gmail.com>
2025-11-26 09:05:19 +0100

    all

---
+++ users/models.py
@@ -0,0 +1,39 @@
+from django.db import models
+from .managers import Manager
+from django.contrib.auth.models import AbstractUser
+from django.core.validators import MinLengthValidator,MaxLengthValidator
+
+# Create your models here.
+class User(AbstractUser):
+    ACCOUNT_TYPE_CHOICES =(
+    ("current", "Current"),
+    ("savings", "Savings"),
+    
+     )
+  
+  
+  
+  
+    email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    username = None
+    bvn = models.CharField(max_length=11)
+    nin = models.CharField(max_length=11)
+    phone_number = models.CharField(max_length=11)
+    date_of_birth = models.DateField(blank=True, null=True)
+    
+    account_number = models.CharField(max_length=10)
+    account_type = models.CharField(max_length=20, choices=ACCOUNT_TYPE_CHOICES)
+    amount = models.FloatField(blank=True, null=True)
+    create_at = models.DateTimeField(auto_now_add=True)
+    
+    
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+    
+    objects = Manager ()
+    
+    
+    def __str__(self):
+        return self.email
